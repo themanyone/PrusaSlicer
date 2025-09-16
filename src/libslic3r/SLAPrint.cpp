722c4e64f59bedc51b531295725214bb5252f4b0
themanyone <nospam@thenerdshow.com>
2025-09-16 06:02:13 -0800

    SLAPrint.cpp: using non-reference type to prevent copying

--- src/libslic3r/SLAPrint.cpp
+++ src/libslic3r/SLAPrint.cpp
@@ -674,7 +674,7 @@ std::string SLAPrint::validate(std::vector<std::string>*) const
     if (iexpt_cur < iexpt_min || iexpt_cur > iexpt_max)
         return _u8L("Initial exposition time is out of printer profile bounds.");
 
-    for (const std::string& prefix : { "", "branching" }) {
+    for (const std::string prefix : { "", "branching" }) {
 
         double head_penetration = m_full_print_config.opt_float(prefix + "support_head_penetration");
         double head_width       = m_full_print_config.opt_float(prefix + "support_head_width");
